3f4d51698f9adc7bf9a1e6bd8861cc41224da808
Michael Taylor <mgt16@lanl.gov>
2023-09-13 10:53:04 -0600

    Ensure charge/multiplictiy assigned in NEB.

--- architector/io_neb.py
+++ architector/io_neb.py
@@ -11,6 +11,14 @@ from architector.io_molecule import convert_io_molecule
 
 def NEB_setup(initial,final,nimages=8,neb_method='GFN2-xTB',interpolation='idpp',climb=True):
     initial_mol = convert_io_molecule(initial)
+    charges = initial_mol.ase_atoms.get_initial_charges()
+    magmoms = initial_mol.ase_atoms.get_initial_magnetic_moments()
+    if 'xtb' in neb_method.lower():
+        charges[0] = initial_mol.xtb_charge
+        magmoms[0] = initial_mol.xtb_uhf
+    else:
+        charges[0] = initial_mol.charge
+        magmoms[0] = initial_mol.uhf
     final_mol = convert_io_molecule(final)
     final_mol_ase = neb_align(initial_mol.ase_atoms,final_mol.ase_atoms)
     final_mol.ase_atoms = final_mol_ase
@@ -21,6 +29,8 @@ def NEB_setup(initial,final,nimages=8,neb_method='GFN2-xTB',interpolation='idpp'
     neb.interpolate(method=interpolation)
     neb_images = []
     for image in neb.images:
+        image.set_initial_magnetic_moments(magmoms)
+        image.set_initial_charges(charges)
         out = CalcExecutor(image,method=neb_method)
         neb_images.append(out.mol.ase_atoms)
     neb.images = neb_images
